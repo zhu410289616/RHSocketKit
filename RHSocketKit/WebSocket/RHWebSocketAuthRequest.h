cd4cb915c55d29e5664b35a229210b74eb037d5c
zhuruhong <zhuruhong@didichuxing.com>
2016-05-17 17:38:27 +0800

    http://www.cnblogs.com/smark/archive/2012/11/26/2789812.html

---
+++ RHSocketKit/WebSocket/RHWebSocketAuthRequest.h
@@ -0,0 +1,13 @@
+//
+//  RHWebSocketAuthRequest.h
+//  RHSocketKitDemo
+//
+//  Created by zhuruhong on 16/5/17.
+//  Copyright © 2016年 zhuruhong. All rights reserved.
+//
+
+#import "RHSocketPacketContext.h"
+
+@interface RHWebSocketAuthRequest : RHSocketPacketRequest
+
+@end
